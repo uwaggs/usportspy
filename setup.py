df8674f5b651d2c91d1b929c3b554762bce449bc
rithikasilva <rithikasilva@outlook.com>
2024-08-27 23:27:32 -0400

    Incremented Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('PYPI.md', 'r') as f:
 
 setup(
     name='usportspy',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     install_requires=[
         'requests',
